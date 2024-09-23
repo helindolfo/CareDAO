821db7cfdc07be29b30cc4d3cbe890d31e7e5f9e
helindolfo <157731907+helindolfo@users.noreply.github.com>
2024-09-23 11:57:39 -0300

    Update CareToken.sol

--- contracts/CareToken.sol
+++ contracts/CareToken.sol
@@ -1,1 +1,71 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.7;
+
+//ERC Token Standard #20 Interface
+interface ERC20Interface {
+    function totalSupply() external view returns (uint);
+    function balanceOf(address tokenOwner) external view returns (uint balance);
+    function allowance(address tokenOwner, address spender) external view returns (uint remaining);
+
+    function transfer(address to, uint tokens) external returns (bool success);
+    function approve(address spender, uint tokens) external returns (bool success);
+    function transferFrom(address from, address to, uint tokens) external returns (bool success);
+ 
+    event Transfer(address indexed from, address indexed to, uint tokens);
+    event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
+}
+ 
+// Token de Governança CARE
+contract CareToken is ERC20Interface {
+    string public symbol = "CARE";
+    string public name = "Care Governance Token";
+    uint8 public decimals = 2;
+    uint256 public _totalSupply;
+
+    mapping(address => uint) balances;
+    mapping(address => mapping(address => uint)) allowed;
+
+    constructor() {
+        _totalSupply = 10000000; // 10.000.000 tokens
+        balances[msg.sender] = _totalSupply; // Todos os tokens atribuídos ao deployer inicialmente
+    }
+
+    function totalSupply() public override view returns (uint256) {
+        return _totalSupply;
+    }
+
+    function balanceOf(address tokenOwner) public override view returns (uint256) {
+        return balances[tokenOwner];
+    }
+
+    function transfer(address to, uint tokens) public override returns (bool success) {
+        require(tokens <= balances[msg.sender], "Saldo insuficiente");
+
+        balances[msg.sender] -= tokens;
+        balances[to] += tokens;
+        emit Transfer(msg.sender, to, tokens);
+        return true;
+    }
+
+    function approve(address spender, uint256 tokens) public override returns (bool success) {
+        allowed[msg.sender][spender] = tokens;
+        emit Approval(msg.sender, spender, tokens);
+        return true;
+    }
+
+    function allowance(address tokenOwner, address spender) public override view returns (uint remaining) {
+        return allowed[tokenOwner][spender];
+    }
+
+    function transferFrom(address from, address to, uint256 tokens) public override returns (bool success) {
+        require(tokens <= balances[from], "Saldo insuficiente");
+        require(tokens <= allowed[from][msg.sender], "Tokens permitidos insuficientes");
+
+        balances[from] -= tokens;
+        allowed[from][msg.sender] -= tokens;
+        balances[to] += tokens;
+        emit Transfer(from, to, tokens);
+        return true;
+    }
+}
 
